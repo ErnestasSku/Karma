87bdf8372e40aae580ff56157e849d9046fdadbf
ErnestasSku <ernestassku@gmail.com>
2021-10-07 20:55:46 +0300

    Add validation tests

--- Karma/UnitTests/StringTests.cs
+++ Karma/UnitTests/StringTests.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using BusinessLogic;
 using BusinessLogic.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = NUnit.Framework.Assert;
 
 namespace UnitTests
 {
@@ -12,6 +14,7 @@ namespace UnitTests
         }
 
         [Test]
+        [TestCategory("StringDistance")]
         public void LevenshteinDistance1()
         {
             string first = "climax";
@@ -22,6 +25,7 @@ namespace UnitTests
         }
 
         [Test]
+        [TestCategory("StringDistance")]
         public void LevenshteinDistance2()
         {
             string first = "Ram";
@@ -32,6 +36,7 @@ namespace UnitTests
         }
 
         [Test]
+        [TestCategory("StringDistance")]
         public void LevenshteinDistance3()
         {
             string first = "kitten";
@@ -40,6 +45,79 @@ namespace UnitTests
             int dist = first.LevenshteinDistance(second);
             Assert.IsTrue(dist == 1);
         }
+
+        [Test]
+        [TestCategory("Email")]
+        public void isEmail1()
+        {
+            string mockEmail = "example@email.com";
+
+            Assert.IsTrue(ValidationUtils.isEmail(mockEmail));
+        }
+
+        [Test]
+        [TestCategory("Email")]
+        public void isEmail2()
+        {
+            string mockEmail = "Name.lastName123@email.com";
+
+            Assert.IsTrue(ValidationUtils.isEmail(mockEmail));
+        }
+
+        [Test]
+        [TestCategory("Email")]
+        public void isEmail3()
+        {
+            string mockEmail = "notAnEmail.com";
+
+            Assert.IsFalse(ValidationUtils.isEmail(mockEmail));
+        }
+
+        [Test]
+        [TestCategory("PhoneNumber")]
+        public void IsPhoneNumber1()
+        {
+            string mockPhoneNumber = "86123456";
+
+            Assert.IsTrue(ValidationUtils.isPhoneNumber(mockPhoneNumber));
+        }
+
+        [Test]
+        [TestCategory("PhoneNumber")]
+        public void IsPhoneNumber2()
+        {
+            string mockPhoneNumber = "+370123456";
+
+            Assert.IsTrue(ValidationUtils.isPhoneNumber(mockPhoneNumber));
+        }
+
+        [Test]
+        [TestCategory("PhoneNumber")]
+        public void IsPhoneNumber3()
+        {
+            string mockPhoneNumber = "notphonenumber";
+
+            Assert.IsFalse(ValidationUtils.isPhoneNumber(mockPhoneNumber));
+        }
+
+        [Test]
+        [TestCategory("Password")]
+        public void IsPassword1()
+        {
+            string mockPassword = "Password123!@#";
+
+            Assert.IsTrue(ValidationUtils.isPassword(mockPassword));
+        }
+
+        [Test]
+        [TestCategory("Password")]
+        public void IsPassword2()
+        {
+            string mockPassword = "short";
+
+            Assert.IsFalse(ValidationUtils.isPassword(mockPassword));
+        }
+
 
     }
 }
