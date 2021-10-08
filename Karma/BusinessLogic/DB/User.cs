de76f8663c5ec8955a7bd6b2679e3f75bfa6fbf8
Tadas Verbyla <90239993+TadasVerbyla@users.noreply.github.com>
2021-10-08 14:24:11 +0300

    Implemented UserData.cs

--- Karma/BusinessLogic/DB/User.cs
+++ Karma/BusinessLogic/DB/User.cs
@@ -10,17 +10,14 @@ namespace BusinessLogic
     /// TODO: UserProfile will be a singleton of UserData? or maybe this is not even needed.
     /// TODO: figure out how the UserProfile will work.
     /// </summary>
-    class User
+    public class User
     {
         string Name { get; set; }
         string LastName { get; set; }
-
         string UserName { get; set; }
-
         string password { get; set; }
 
         //Stuff like House Adress, Phone number (they can also be optional if people don't want to give their data)
-
         
         //Token might be useful for authentification stuff maybe? 
         //string Token;
