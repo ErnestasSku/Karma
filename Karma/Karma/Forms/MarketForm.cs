7d1a3e45354856113d81bf1c7392e91f4dedb89e
scasm <justas.stonkus28@gmail.com>
2021-10-10 18:09:33 +0300

    Added filtering by description and title, also fixed a minor bug in previous PR

--- Karma/Karma/Forms/MarketForm.cs
+++ Karma/Karma/Forms/MarketForm.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Windows.Forms;
 using BusinessLogic;
+using System.Collections.Generic;
 
 namespace Karma
 {
@@ -12,15 +13,13 @@ namespace Karma
         public MarketForm()
         {
             InitializeComponent();
-            string[] items = { "Name A-Z", "Name Z-A", "Newest", "Oldest" };
+            string[] items = { "Name A-Z", "Name Z-A", "Oldest", "Newest" };
             comboBox1.Items.AddRange(items);
         }
 
 
         private void AddNewButtonClick(object sender, EventArgs e)
-        {
-
-            
+        {       
             Form addNewItemForm = new AddNewItemForm();
 
             addNewItemForm.TopLevel = false;
@@ -47,12 +46,15 @@ namespace Karma
 
             if (CSVProcessing.Items == null)
                 return;
+
             foreach (var item in CSVProcessing.Items)
             {
+               
                 ItemLayoutPanel.Controls.Add(new UserControlItem(item));
+                
             }
-
         }
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -65,6 +67,18 @@ namespace Karma
 
             BusinessLogic.Utils.ItemUtilities.SortItems(CSVProcessing.Items, BusinessLogic.Utils.ItemUtilities.GetSortType(selected), descending);
             this.InvokePaint(this, new PaintEventArgs(this.CreateGraphics(), this.DisplayRectangle));
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            foreach(UserControlItem uc in ItemLayoutPanel.Controls.OfType<UserControlItem>())
+            {
+                if (!uc.TitleLabel.Text.Contains(textBox1.Text) && !uc.DescriptionLabel.Text.Contains(textBox1.Text))
+                    uc.Visible = false;
+                else
+                    uc.Visible = true;
+
+            }    
         }
     }
 }
