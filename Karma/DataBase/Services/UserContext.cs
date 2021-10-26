cb8fbc33bd82c77375e15a3b1f79973d62ef055e
Tadas Verbyla <90239993+TadasVerbyla@users.noreply.github.com>
2021-10-26 13:05:30 +0300

    Added User.cs and UserContext.cs or DB

---
+++ Karma/DataBase/Services/UserContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Backend;
+using Xamarin.Essentials;
+using DataBase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBase.Services
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserContext()
+        {
+            SQLitePCL.Batteries_V2.Init();
+
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "Users.db");
+
+            optionsBuilder.UseSqlite($"Filename={dbPath}");
+        }
+    }
+}
