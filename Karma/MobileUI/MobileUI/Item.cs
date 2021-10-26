9e7b59915c4d5f74d02a4b68421e883165a0c4da
scasm <justas.stonkus28@gmail.com>
2021-10-26 05:32:44 +0300

    Add Xamarin.forms project with some UI

---
+++ Karma/MobileUI/MobileUI/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileUI
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string ImgSource { get; set; }
+    }
+}
---
+++ Karma/MobileUI/MobileUI/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Item_Tapped(object sender, System.EventArgs e)
+        {
+            App.Current.MainPage.Navigation.PushAsync(new ItemDetailPage());
+
+            //throw new Exception();
+        }
+    }
+}
