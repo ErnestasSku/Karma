728a65339ecdddf95052af3184ff16ef3660f9f5
Justas Stonkus <justas.stonkus28@gmail.com>
2021-11-16 20:34:48 +0200

    start page added

--- Karma/MobileUI/MobileUI/App.xaml.cs
+++ Karma/MobileUI/MobileUI/App.xaml.cs
@@ -15,7 +15,7 @@ namespace MobileUI
             //FOR DEBUGING VARIOUS PAGES, CHANGE < new ...Page(); > into required page to view it
             Item.Items = new MvvmHelpers.ObservableRangeCollection<Item>();
             Item.AddData(Item.Items);
-            MainPage = new TabbedPage1();
+            MainPage = new StartPage();
             
             //Current.MainPage = new NavigationPage(FirstPage);
         }
