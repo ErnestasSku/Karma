65eab6ba688871e2d083f3cf7d64af93bd8248c5
Justas Stonkus <justas.stonkus28@gmail.com>
2021-11-30 12:31:10 +0200

    Added sorting

--- Karma/MobileUI/MobileUI/Views/MarketPage.xaml.cs
+++ Karma/MobileUI/MobileUI/Views/MarketPage.xaml.cs
@@ -36,5 +36,10 @@ namespace MobileUI.Views
             await ItemPage.Navigation.PushAsync(new ItemDetailPage());
         }
 
+        private void SortType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            MarketPageViewModel.Sort(SortType.SelectedIndex);
+        }
     }
 }
