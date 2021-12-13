e11a6ae0d3b7d34a2d81f508b6d06199f8ed433c
ErnestasSku <ernestassku@gmail.com>
2021-12-14 00:29:35 +0200

    Nuget purge.

--- Karma/DataBaseTests/ItemServiceTests.cs
+++ Karma/DataBaseTests/ItemServiceTests.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.InMemory;
 using System.Linq;
-using System.Data.Entity.Infrastructure;
 
 namespace DataBaseTests
 {
